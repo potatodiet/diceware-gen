2ee9a379e34bc6e725504cebdb85117124b612cf
Justin Harrison <me@justinharrison.ca>
2017-02-25 06:40:59 -0500

    Split into reusable library

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+extern crate rand;
+
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+use rand::{thread_rng, Rng};
+
+#[derive(Debug)]
+pub enum DiceError {
+    Io(io::Error),
+}
+
+impl From<io::Error> for DiceError {
+    fn from(err: io::Error) -> DiceError {
+        DiceError::Io(err)
+    }
+}
+
+
+pub struct DicewareGen {
+    words: Vec<String>,
+    rng: rand::ThreadRng,
+}
+
+impl DicewareGen {
+    pub fn new() -> Result<DicewareGen, DiceError> {
+        Ok(DicewareGen {
+            words: {
+                let f = File::open("eff_large_wordlist.txt")?;
+                BufReader::new(f).lines().map(|line| {
+                    line
+                }).collect::<Result<Vec<String>, io::Error>>()?
+            },
+            rng: thread_rng(),
+        })
+    }
+
+    pub fn gen(mut self, rounds: u8) -> Vec<String> {
+        (0..rounds).map(|_| {
+            let x: usize = self.rng.gen_range(0, 7775);
+            self.words[x].clone()
+        }).collect::<Vec<String>>()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
-extern crate rand;
+extern crate diceware_gen;
 
+use diceware_gen::DicewareGen;
 use std::env;
-use std::io::BufReader;
-use std::io::prelude::*;
-use std::fs::File;
-use rand::{thread_rng, Rng};
 
 fn main() {
-    let f = File::open("eff_large_wordlist.txt").expect("wordlist doesn't exist");
-
-    let mut words: Vec<String> = Vec::with_capacity(7776);
-    for line in BufReader::new(f).lines() {
-        words.push(line.unwrap());
-    }
+    let dice = DicewareGen::new().unwrap();
 
     let rounds: u8 = env::args().nth(1).expect("must pass number of rounds as argument")
-                                       .parse().expect("could not convert argument to u8");
-    let mut rng = thread_rng();
-    for _ in 0..rounds {
-        let x: usize = rng.gen_range(0, 7775);
-        println!("{}", words[x]);
+                                .parse().expect("could not convert argument to u8");
+    for word in dice.gen(rounds) {
+        println!("{}", word);
     }
 }
